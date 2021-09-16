fb2fba3d32960ba2a7adeec79d0be357b123ca5c
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2021-09-16 09:14:51 -0500

    Update news_controller.dart

--- lib/domain/controllers/news_controller.dart
+++ lib/domain/controllers/news_controller.dart
@@ -1,6 +1,9 @@
+import 'package:f_web_service_template/data/model/news_item.dart';
 import 'package:get/get.dart';
 
 class NewsController {
+  var _news = <NewsItem>[].obs;
+  var _loading = false.obs;
   // functions getNews() and reset()
 
 }
