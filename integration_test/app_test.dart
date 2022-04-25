d2078838a0c09cc5cf9444079e918fbbe0b579ff
Augusto Salazar SIlva <augustosalazar@uninorte.edu.co>
2022-04-25 08:10:47 -0500

    Moved repository and add DI

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,6 +1,6 @@
-import 'package:f_web_service_template/data/data_repository.dart';
-import 'package:f_web_service_template/domain/controllers/news_controller.dart';
+import 'package:f_web_service_template/domain/repositories/data_repository.dart';
 import 'package:f_web_service_template/domain/use_case/news.dart';
+import 'package:f_web_service_template/ui/controllers/news_controller.dart';
 import 'package:f_web_service_template/ui/my_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -28,30 +28,31 @@ void main() {
     Widget w = await createHomeScreen();
     await tester.pumpWidget(w);
 
-    expect(find.byKey(Key('footballNewsButton')), findsOneWidget);
+    expect(find.byKey(const Key('footballNewsButton')), findsOneWidget);
 
-    await tester.tap(find.byKey(Key('footballNewsButton')));
+    await tester.tap(find.byKey(const Key('footballNewsButton')));
 
     await tester.pumpAndSettle();
 
-    expect(find.byKey(Key('readMoreButton')), findsWidgets);
+    expect(find.byKey(const Key('readMoreButton')), findsWidgets);
 
     // Therefore: (The command Offset uses Cartesian 'directions') - lets see: a) Left Dragging: Offset(-500.0, 0.0) b) Right Dragging: Offset(+500.0, 0.0) c) Up Dragging: Offset(0.0, +500.0) d) Down Dragging: Offset(0.0, -500.0)
-    await tester.drag(find.byKey(Key('refreshList')), const Offset(0.0, 500.0));
+    await tester.drag(
+        find.byKey(const Key('refreshList')), const Offset(0.0, 500.0));
 
     await tester.pumpAndSettle();
 
     await tester.pumpAndSettle();
 
-    expect(find.byKey(Key('readMoreButton')), findsWidgets);
+    expect(find.byKey(const Key('readMoreButton')), findsWidgets);
 
     await tester.drag(
-        find.byKey(Key('dragToResetGesture')), const Offset(500.0, 0.0));
+        find.byKey(const Key('dragToResetGesture')), const Offset(500.0, 0.0));
 
     await tester.pumpAndSettle();
 
     await tester.pumpAndSettle();
 
-    expect(find.byKey(Key('readMoreButton')), findsNothing);
+    expect(find.byKey(const Key('readMoreButton')), findsNothing);
   });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'package:f_web_service_template/domain/repositories/data_repository.dart';
+import 'package:f_web_service_template/ui/controllers/news_controller.dart';
 import 'package:f_web_service_template/ui/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:loggy/loggy.dart';
 
+import 'domain/use_case/news.dart';
+
 void main() {
+  // add reposiroty, user case and repository to DI
+  Get.put(DataRepository());
+  Get.put(News());
+  Get.put(NewsController());
+
   Loggy.initLoggy(
     logPrinter: const PrettyPrinter(
       showColors: true,
